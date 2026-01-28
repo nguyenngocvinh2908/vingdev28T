f085d24610d7af571aaa50adcb768f137e7006dc
Vinh Ga <nguyenngocvinh770@gmai.com>
2026-01-28 16:15:39 +0700

    Fix 03

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ dataBase.connect()
 
 // Pug Setup
 app.set('view engine', 'pug')
-app.set('views', `${__dirname}/views`)
+app.set('views', __dirname + '/views')
 
 // Public Folder Setup
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(__dirname + '/public'))
 
 // Routes Setup
 const clientRoutes = require('./routes/client/index')
